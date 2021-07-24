597f3b7e5752f52ccd4fcc91258db247de465f9a
shoaib <p200147@pwr.nu.edu.pk>
2021-07-25 04:08:56 +0500

    Basic Java Programs

---
+++ basicPrograms/helloWorld.java
@@ -0,0 +1,12 @@
+package ist;
+
+public class firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
