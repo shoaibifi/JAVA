e6828d14035770464b2a8e680fffbd95d98de885
shoaib <p200147@pwr.nu.edu.pk>
2021-07-25 04:31:47 +0500

    Variables in java

---
+++ basicPrograms/variables.java
@@ -0,0 +1,20 @@
+package ist;
+
+public class variables {
+
+	public static void main(String[] args) {
+	
+	int age = 18; // its an integer data type 
+	float averageMarks = 19.765f ; // we have to write f at end of the the the digit after writing it only then java can understand it that it's a float
+	char grade = 'A'; // its a character data type
+	String name = "Shoaib Akhtar"; // it's a string data type
+	boolean valid = false; // its a boolean data type we can only assign "True" or "False" to it
+
+	// here we are printing the values of the variables
+	System.out.println(valid);
+	System.out.println(age);
+	System.out.println(grade);
+	System.out.println(name);
+	}
+
+}
